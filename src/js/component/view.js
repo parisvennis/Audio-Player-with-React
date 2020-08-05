215eb52a25afbd7733bb62804fdc22ed8f14f749
parisvennis <parisvennis@gmail.com>
2020-08-05 20:05:48 +0000

    update

--- src/js/component/view.js
+++ src/js/component/view.js
@@ -4,6 +4,7 @@ export class View extends React.Component {
 	constructor() {
 		super();
 		this.audio = null;
+
 		this.state = {
 			currentIndex: 1,
 			songs: [
@@ -32,6 +33,13 @@ export class View extends React.Component {
 		};
 	}
 
+	componentDidMount() {
+		this.pauseButton.style.display = "none";
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then(response => response.json())
+			.then(songs => this.setState({ songs }));
+	}
+
 	changeTrack(i) {
 		this.setState({ currentIndex: i });
 		this.audio.current.pause();
@@ -39,24 +47,61 @@ export class View extends React.Component {
 		this.audio.current.play();
 	}
 
+	play = i => {
+		var url = this.state.songs[i].url;
+		const songURL = "https://assets.breatheco.de/apis/sound/" + url;
+		this.audio.src = songURL;
+		this.audio.play();
+		this.playButton.style.display = "none";
+		this.pauseButton.style.display = "inline-block";
+		this.setState({ currentIndex: i });
+	};
+
+	pause = () => {
+		this.audio.pause();
+		this.playButton.style.display = "inline-block";
+		this.pauseButton.style.display = "none";
+	};
+
 	render() {
 		const liList = this.state.songs.map((song, index) => {
 			return (
-				<li key={index} onClick={() => this.changeTrack(index)}>
+				<li
+					key={index}
+					onClick={() => this.play(this.state.currentIndex)}>
 					<span>{index + 1}</span>
-					<span>{song.title}</span>
+					<span>{song.name}</span>
 				</li>
 			);
 		});
 
 		const audioPlayer = (
-			<audio controls ref={element => (this.audio = element)}>
-				<source
-					src={this.state.songs[this.state.currentIndex].url}
-					type="audio/mp3"
-				/>
-			</audio>
+			<>
+				<div>
+					<button
+						onClick={() => this.play(this.state.currentIndex - 1)}>
+						<i className="fa fa-caret-left" aria-hidden="true" />
+					</button>
+					<button
+						ref={element => (this.playButton = element)}
+						onClick={() => this.play(this.state.currentIndex)}>
+						<i className="fa fa-play" aria-hidden="true" />
+					</button>
+					<button
+						ref={element => (this.pauseButton = element)}
+						onClick={() => this.pause(this.state.currentIndex)}>
+						<i className="fa fa-pause" aria-hidden="true" />
+					</button>
+					<button
+						onClick={() => this.play(this.state.currentIndex + 1)}>
+						<i className="fa fa-caret-right" aria-hidden="true" />
+					</button>
+				</div>
+
+				<audio controls ref={element => (this.audio = element)} />
+			</>
 		);
+
 		return (
 			<>
 				{audioPlayer}
