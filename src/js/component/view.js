ec7d8374b8733db99c4cedc301011c422b729a2c
parisvennis <parisvennis@gmail.com>
2020-08-05 15:54:12 +0000

    updates

--- src/js/component/view.js
+++ src/js/component/view.js
@@ -3,12 +3,53 @@ import React from "react";
 export class View extends React.Component {
 	constructor() {
 		super();
-		this.state = {};
+		this.state = {
+			currentIndex: 0,
+			songs: [
+				{
+					title: "South Park",
+					id: "south-park",
+					author: "Kyle",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/south-park.mp3"
+				},
+				{
+					title: "Thunder Cats",
+					id: "thundercats",
+					author: "Moonra",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/thundercats.mp3"
+				},
+				{
+					title: "X-Men",
+					id: "x-men",
+					author: "Profesor",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/x-men.mp3"
+				}
+			]
+		};
 	}
 	render() {
+		const liList = this.state.songs.map((song, index) => {
+			return (
+				<li key={index}>
+					<span>
+						<span>{index + 1}</span>
+						{song.title}
+					</span>
+				</li>
+			);
+		});
 		return (
 			<>
-				<audio controls />
+				<audio controls>
+					<source
+						src="https://assets.breatheco.de/apis/sound/files/cartoons/songs/x-men.mp3"
+						type="audio/mp3"
+					/>
+				</audio>
+				<ul>{liList}</ul>
 			</>
 		);
 	}
